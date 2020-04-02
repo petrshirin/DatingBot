dab6ef2c323e090f524b84e79d01ec9fe7eba55a
Petr <p.e.shirin@gmail.com>
2020-04-02 15:07:35 +0800

    add CASCADE

--- userprofile/models.py
+++ userprofile/models.py
@@ -14,7 +14,7 @@ class Chat(models.Model):
 
 
 class UserProfile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.SET_NULL, default=None, null=True, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default=None, null=True, blank=True)
     first_name = models.CharField(max_length=255, default=None, blank=True, null=True)
     second_name = models.CharField(max_length=255, default=None, blank=True, null=True)
     photo = models.FileField(default=None, null=True, blank=True, upload_to='image/profile/')
@@ -23,7 +23,7 @@ class UserProfile(models.Model):
     status = models.TextField(default=None, null=True, blank=True)
     phone = models.CharField(max_length=20, default=None, null=True, blank=True)
     restaurant = models.ForeignKey(UserRestaurant, on_delete=models.CASCADE, default=None, null=True, blank=True)
-    chat = models.OneToOneField(Chat, on_delete=models.SET_NULL, default=None, null=True, blank=True)
+    chat = models.OneToOneField(Chat, on_delete=models.CASCADE, default=None, null=True, blank=True)
     is_active = models.BooleanField(default=True)
     search_for = models.CharField(max_length=20, default=None, null=True, blank=True)
 
@@ -32,15 +32,15 @@ class UserProfile(models.Model):
 
 
 class UserView(models.Model):
-    user_profile = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, related_name='+', null=True, blank=True)
-    view_user = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, related_name='+', null=True, blank=True)
+    user_profile = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='+', null=True, blank=True)
+    view_user = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='+', null=True, blank=True)
     result = models.BooleanField(default=True)
     date = models.DateField(auto_now=True)
 
 
 class UserCoincidence(models.Model):
-    user_1 = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, related_name='+', null=True, blank=True)
-    user_2 = models.ForeignKey(UserProfile, on_delete=models.SET_NULL, related_name='+', null=True, blank=True)
+    user_1 = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='+', null=True, blank=True)
+    user_2 = models.ForeignKey(UserProfile, on_delete=models.CASCADE, related_name='+', null=True, blank=True)
     is_view_1 = models.BooleanField(default=False)
     is_view_2 = models.BooleanField(default=False)
 
