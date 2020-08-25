fea693157efaa64fc39f968c6d7d2be5d8ae106c
Petr <p.e.shirin@gmail.com>
2020-08-25 15:51:46 +0800

    add redirect non authenticated users

--- userprofile/middleware.py
+++ userprofile/middleware.py
@@ -23,7 +23,7 @@ class CheckUserPhotoMiddleware(MiddlewareMixin):
     def process_request(self, request):
         if request.user.is_authenticated:
 
-            if not request.user.userprofile.photo and 'addphoto' not in request.path:
+            if not request.user.userprofile.photo and 'add' not in request.path:
                 if not request.user.is_staff:
                     return redirect('/profile/addphoto/')
         else:
