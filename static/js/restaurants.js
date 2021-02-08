1d32e871325cb313be37257022570d63dd182ee7
Petr <p.e.shirin@gmail.com>
2021-02-08 18:04:28 +0800

    delete third restaurant

--- static/js/restaurants.js
+++ static/js/restaurants.js
@@ -7,7 +7,6 @@ function change_rest(rest_id) {
     if (request.readyState === 4 && request.status === 200) {
         document.getElementById("choice1").checked=false;
         document.getElementById("choice2").checked=false;
-        document.getElementById("choice3").checked=false;
         document.getElementById("choice"+rest_id).checked=true;
         console.log(request.response);
         localStorage.setItem('restaurant_id', rest_id)
